87cbe43ef2f37e6b3d444fbdcbb0ac9977c373e5
yixi-sha <shazb171318515@gmail.com>
2020-08-11 17:31:30 +0800

    tmp end

--- wrap_func.c
+++ wrap_func.c
@@ -1,17 +1,10 @@
-#include <sys/types.h>          /* See NOTES */
-#include <sys/socket.h>
-#include <pthread.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#define WRAPFUNC
 #include "wrap_func.h"
-#include <signal.h>
 
 
+
+
+int err_flag = 0;
 
 int Socket(int family, int type, int protocol){
     int n;
@@ -57,7 +50,7 @@ int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen){
     return n;
 }
 
-int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen){ // don`t right if erron == EINTR close then connect
     int n;
     if((n = connect(sockfd, addr, addrlen)) < 0)
         err("connect");
@@ -82,10 +75,10 @@ int Inet_pton(int af, const char *src, void *dst){
 }
 
 char *Sock_ntop(const struct sockaddr *sockaddrIn, socklen_t len){
-    static char str[128];
+    static char str[512];
     char potrSrt[8];
     struct sockaddr_in *ipv4 = NULL;
-
+    struct sockaddr_in6 *ipv6 = NULL;
     switch(sockaddrIn->sa_family){
         case AF_INET:
             ipv4 = (struct sockaddr_in *)sockaddrIn;
@@ -94,9 +87,19 @@ char *Sock_ntop(const struct sockaddr *sockaddrIn, socklen_t len){
             if(ntohs(ipv4->sin_port) != 0){
                 snprintf(potrSrt, sizeof(potrSrt), ":%d", ntohs(ipv4->sin_port));
                 strcat(str, potrSrt);
-                
             }
             return str;
+        case AF_INET6:
+            ipv6 = (struct sockaddr_in6 *)sockaddrIn;
+            if(inet_ntop(ipv6->sin6_family, &ipv6->sin6_addr, str, 128) == NULL)
+                return NULL;
+            if(ntohs(ipv6->sin6_port) != 0){
+                snprintf(potrSrt, sizeof(potrSrt), ":%d", ntohs(ipv6->sin6_port));
+                strcat(str, potrSrt);
+            }
+            return str;
+        default:
+            break;
     }
 
     return str;
@@ -270,7 +273,7 @@ Signal_func* Signal(int signalNum, Signal_func* signal_func){
     if(sigaction(signalNum, &newAtion, &oldAction) < 0)
         err("sigaction");
     
-    printf("signal end\n");
+    //printf("signal end\n");
     return oldAction.sa_handler;
 
 }
@@ -470,8 +473,7 @@ int Udp_client(const char *hostname, const char *service, struct sockaddr **sapt
             *lenp = addrinfoRet->ai_addrlen;
             break;
         }
-            
-        
+
         clientFd = -1;
         addrinfoRet = addrinfoRet->ai_next;
     }
@@ -509,7 +511,7 @@ int Udp_client_connect(const char *hostname, const char *service, int family){
 
 int Udp_server(const char *hostname, const char *service, socklen_t *plen, int family){
     int clientFd = -1;
-
+    const int on = 1;
     struct addrinfo *addrinfoRet = NULL, *addrinfoHead = NULL;
 
     addrinfoRet = Host_serv(hostname, service, family, SOCK_DGRAM, AI_PASSIVE);
@@ -525,9 +527,9 @@ int Udp_server(const char *hostname, const char *service, socklen_t *plen, int f
         }
         if(bind(clientFd, addrinfoRet->ai_addr, addrinfoRet->ai_addrlen) == 0){
             printf("bind %s\n", Sock_ntop(addrinfoRet->ai_addr, addrinfoRet->ai_addrlen));
+            Setsockopt(clientFd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
             break;
-        } 
-        
+        }
         clientFd = -1;
         addrinfoRet = addrinfoRet->ai_next;
     }
@@ -565,8 +567,490 @@ int daemon_init(const char *pname, int faclity){
     open("/dev/null", O_RDONLY);
     open("/dev/null", O_RDWR);
     open("/dev/null", O_RDWR);
-
+    err_flag = 1;
     openlog(pname, LOG_PID, faclity);
 
     return 0;
 }   
+
+static void connect_alarm(){
+    return;
+}
+
+int connect_timeo(int socketFd, struct sockaddr *addr, socklen_t alen, int nsec){
+    Signal_func *sigFunc;
+    int ret;
+
+    sigFunc = Signal(SIGALRM, connect_alarm);
+    
+    if(alarm(nsec) != 0){
+        err_msg("alarm aleady set\n");
+    }
+
+    if((ret = connect(socketFd, addr, alen)) < 0){
+        close(socketFd);
+        if(errno == EINTR)
+            errno = ETIMEDOUT;
+    }
+    alarm(0);
+    Signal(SIGALRM, sigFunc);
+
+    return ret;
+}
+
+int Readable_timeo(int fd, int sec){
+    fd_set rset;
+    struct timeval tv;
+    int ret;
+
+    FD_ZERO(&rset);
+    FD_SET(fd, &rset);
+
+    tv.tv_sec = sec;
+    tv.tv_usec = 0;
+    if((ret = select(fd + 1, &rset, NULL, NULL, &tv)) > 0)
+        return 0;
+    else if (ret == 0)
+        return 1;
+    else
+        return ret;
+}
+
+int Socketpair(int domain, int type, int protocol, int sv[2]){
+    while(1){
+        if(socketpair(domain, type, protocol, sv) == 0)
+            break;
+        else{
+            if(errno == EINTR){
+                continue;
+            }
+            else{
+                err("socketpair error");
+            }
+        }
+    }
+    return 0;
+}
+
+ssize_t Recvmsg(int sockfd, struct msghdr *msg, int flags){
+    int ret;
+    while(1){
+        if((ret = recvmsg(sockfd, msg, flags)) >= 0)
+            break;
+        else{
+            if(errno == EINTR){
+                continue;
+            }
+            else{
+                err("socketpair error");
+            }
+        }
+    }
+    return ret;
+}
+
+ssize_t Sendmsg(int sockfd, const struct msghdr *msg, int flags){
+    int ret;
+    while(1){
+        
+        if((ret = sendmsg(sockfd, msg, flags)) >= 0)
+            break;
+        else{
+            if(errno == EINTR){
+                continue;
+            }
+            else{
+                err("socketpair error");
+            }
+        }
+        
+    }
+    return ret;
+}
+
+
+
+int Connect_nonb(int socketFd, struct sockaddr *addr, socklen_t alen, int nsec){
+    fd_set read, write;
+    struct timeval tval;
+    int n = 0;
+    int flags = fcntl(socketFd, F_GETFL, 0);
+    int error = 0;
+    
+    fcntl(socketFd, F_SETFL, flags | O_NONBLOCK);
+
+    FD_ZERO(&read);
+    FD_ZERO(&write);
+    FD_SET(socketFd, &read);
+    FD_SET(socketFd, &write);
+
+    tval.tv_sec = nsec;
+    tval.tv_usec = 0;
+
+    if((n = connect(socketFd, addr, alen)) == 0){
+        fcntl(socketFd, F_SETFL, flags);
+        return 0;
+    }else if(n < 0){
+        if( errno != EINPROGRESS)
+            return -1;
+    }
+    
+    if((n = Select(socketFd + 1, &read, &write, NULL, nsec == 0 ? NULL : &tval)) == 0){
+        close(socketFd);
+        errno = ETIMEDOUT;
+        return -1;
+    }
+    if (FD_ISSET(socketFd, &read) || FD_ISSET(socketFd, &write)) {
+        int len = sizeof(error);
+        if (getsockopt(socketFd, SOL_SOCKET, SO_ERROR, &error, &len) < 0)
+            return -1; 
+    } else{
+        err("select error: sockfd not set");
+    }
+    
+    if(error){
+        close(socketFd);
+        errno = error;
+        return -1;
+    }
+
+    fcntl(socketFd, F_SETFL, flags);
+    return 0;
+}
+
+int Min(int a, int b){
+    return a > b ? b : a;
+}
+
+
+struct IfiInfo* Get_ifi_info(int family, int doaliases){
+    int sockfd;
+    int len = 0, lastlen;
+    char *buf, lastname[IFI_NAME_LEN], *sdlname = NULL, *ptr, *haddr, *cptr;
+    struct ifconf ifc;
+    struct IfiInfo *ifi, *ifihead, **ifipnext;
+    struct ifreq *ifr, ifrcopy;
+    int flags, myflags, idx, hlen;
+
+    sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+    len = 100 * sizeof(struct ifreq);
+    lastlen = 0;
+    while(1){
+        buf = malloc(len);
+        if(!buf)
+            err("malloc err");
+        ifc.ifc_len = len;
+        ifc.ifc_buf = buf;
+        if(ioctl(sockfd, SIOCGIFCONF, &ifc) < 0){
+            if(errno != EINVAL || lastlen != 0)
+                err("ioctl error");
+        }else{
+            if(ifc.ifc_len == lastlen){
+                break;
+            }
+            lastlen = ifc.ifc_len;        
+        }
+        len += 10 * sizeof(struct ifreq);
+        free(buf);
+    }
+    ifihead = NULL;
+    ifipnext = &ifihead;
+    lastname[0] = 0;
+    sdlname = NULL;
+    
+    for(ptr = buf; ptr < buf + ifc.ifc_len;){
+        ifr = (struct ifreq*)ptr;
+        ptr += sizeof(struct ifreq);
+       
+        if(ifr->ifr_addr.sa_family != family){
+            continue;
+        }
+           
+        myflags = 0;
+        if((cptr = strchr(ifr->ifr_name,':')) != NULL){
+            *cptr = 0;
+        }
+            
+        if(strncmp(lastname, ifr->ifr_name, IF_NAMESIZE) == 0){
+            if(doaliases == 0)
+                continue;
+            myflags = IFI_ALIAS;
+        }
+        memcpy(lastname, ifr->ifr_name, IF_NAMESIZE);
+        ifrcopy = *ifr;
+        ioctl(sockfd, SIOCGIFFLAGS, &ifrcopy);
+        flags = ifrcopy.ifr_flags;
+        if((flags & IFF_UP) == 0)
+            continue;
+        ifi = calloc(1, sizeof(struct IfiInfo));
+        if(ifi == NULL)
+            err("calloc error");
+        *ifipnext = ifi;
+        ifipnext = &ifi->next;
+
+        ifi->flags = flags;
+        ifi->myFlags = myflags;
+
+        
+        ioctl(sockfd, SIOCGIFMTU, &ifrcopy);
+        ifi->mtu = ifrcopy.ifr_mtu;
+        
+        
+        memcpy(ifi->name, ifr->ifr_name, IFI_NAME_LEN);
+        
+        ifi->name[IFI_NAME_LEN - 1] = '\0';
+        if(sdlname == NULL || strcmp(sdlname, ifr->ifr_name) != 0){
+            idx = hlen = 0;
+        }
+        ifi->index = idx;
+        ifi->hlen = hlen;
+        if(ifi->hlen > IFI_HADDR_LEN)
+            ifi->hlen = IFI_HADDR_LEN;
+        if(hlen){
+            memcpy(ifi->haddr, haddr, ifi->hlen);
+        }
+
+        switch(ifr->ifr_addr.sa_family){
+            case AF_INET:
+                ifi->addr = calloc(1, sizeof(struct sockaddr_in));
+                if(ifi->addr == NULL)
+                    err("calloc error");
+                memcpy(ifi->addr, &ifr->ifr_addr, sizeof(struct sockaddr_in));
+
+                
+                if(flags & IFF_BROADCAST){
+                    ioctl(sockfd, SIOCGIFBRDADDR, &ifrcopy);
+                    ifi->baddr = calloc(1, sizeof(struct sockaddr_in));
+                    if(ifi->addr == NULL)
+                        err("calloc error");
+                    memcpy(ifi->baddr, &ifrcopy.ifr_addr, sizeof(struct sockaddr_in));
+                }
+                
+                if(flags & IFF_POINTOPOINT){
+                    ioctl(sockfd, SIOCGIFDSTADDR, &ifrcopy);
+                    ifi->detaddr = calloc(1, sizeof(struct sockaddr_in));
+                    if(ifi->addr == NULL)
+                        err("calloc error");
+                    memcpy(ifi->detaddr, &ifrcopy.ifr_addr, sizeof(struct sockaddr_in));
+                }
+               
+                break;
+
+            case AF_INET6:
+                ifi->addr = calloc(1, sizeof(struct sockaddr_in6));
+                if(ifi->addr == NULL)
+                    err("calloc error");
+                memcpy(ifi->addr, &ifr->ifr_addr, sizeof(struct sockaddr_in6));
+                if(flags & IFF_POINTOPOINT){
+                    ioctl(sockfd, SIOCGIFDSTADDR, &ifrcopy);
+                    ifi->detaddr = calloc(1, sizeof(struct sockaddr_in6));
+                    if(ifi->addr == NULL)
+                        err("calloc error");
+                    memcpy(ifi->detaddr, &ifrcopy.ifr_addr, sizeof(struct sockaddr_in6));
+                }
+                break;
+
+            default:
+                break;
+        }
+
+    }
+    free(buf);
+    return ifihead;
+}
+
+void Free_ifi_info(struct IfiInfo *ifihead){
+    struct IfiInfo *ifi, *ifinext;
+
+    for (ifi = ifihead; ifi != NULL; ifi = ifinext){
+        if(ifi->addr != NULL)
+            free(ifi->addr);
+        if(ifi->baddr != NULL)
+            free(ifi->baddr);
+        if(ifi->detaddr != NULL)
+            free(ifi->detaddr);
+        ifinext = ifi->next;
+        free(ifi);
+    }
+}
+
+int Family_to_level(int family){
+    switch (family) {
+        case AF_INET: {
+            return (IPPROTO_IP);
+        }
+        case AF_INET6: {
+            return (IPPROTO_IPV6);
+        }
+        default: {
+            return (-1);
+        }
+    }
+}
+
+int Mcast_join(int sock, const struct sockaddr *grp, socklen_t grplen, const char *ifname, unsigned int ifindex){
+    
+
+    struct group_req req;
+    if(ifindex > 0){
+        req.gr_interface = ifindex;
+    }else if(ifname){
+        if((req.gr_interface = if_nametoindex(ifname)) == 0){
+            errno = ENXIO;
+            return -1;
+        }
+    }else{
+        req.gr_interface = 0;
+    }
+    if(grplen > sizeof(req.gr_group)){
+        errno = EINVAL;
+        return -1;
+    }
+    memcpy(&req.gr_group, grp, grplen);
+    return setsockopt(sock, Family_to_level(grp->sa_family), MCAST_JOIN_GROUP, &req, sizeof(req));
+}
+
+int Sockfd_to_family(int sockfd){
+    struct sockaddr_storage ss;
+    socklen_t    len;
+
+    len = sizeof(ss);
+    if (getsockname(sockfd, (struct sockaddr *)&ss, &len) < 0)
+        return(-1);
+    return(ss.ss_family);
+}
+
+int Mcast_set_loop(int sock, int onoff){
+    switch (Sockfd_to_family(sock)){
+        case AF_INET:{
+            unsigned char flag = onoff;
+            return setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP, &flag, sizeof(flag));
+        }
+        case AF_INET6:{
+            unsigned int flag = onoff;
+            return setsockopt(sock, IPPROTO_IPV6, IPV6_MULTICAST_LOOP, &flag, sizeof(flag));
+        }
+        default:
+            errno = EAFNOSUPPORT;
+            return -1;
+    }
+}
+
+ssize_t Recv_des(int fd, void *ptr, size_t nbytes,SA *sa, socklen_t *alen,struct in_pktinfo *pktp){
+    struct msghdr msg;
+    struct iovec iov[1];
+    size_t n;
+    int opt = 1;
+
+    struct cmsghdr *cmptr;
+    union{
+        struct cmsghdr cm;
+        char control[CMSG_SPACE(sizeof(struct in_pktinfo)) + CMSG_SPACE(sizeof(int))];
+    }control_un;
+    n = setsockopt(fd, IPPROTO_IP, IP_PKTINFO, &opt, sizeof (opt));
+    if(n < 0)
+        err("setsockopt error");
+    msg.msg_control = control_un.control;
+    msg.msg_controllen = sizeof(control_un.control);
+    msg.msg_flags = 0;
+    msg.msg_name = sa;
+    msg.msg_namelen = *alen;
+    iov[0].iov_base = ptr;
+    iov[0].iov_len = nbytes;
+    msg.msg_iov = iov;
+    msg.msg_iovlen = 1;
+    //msg.msg_control = NULL;
+    //msg.msg_controllen = 0;
+    
+    if((n = recvmsg(fd, &msg, 0)) < 0){
+        printf("!!!\n");
+        return n;
+    }
+    for(cmptr = CMSG_FIRSTHDR(&msg); cmptr != NULL; cmptr = CMSG_NXTHDR(&msg, cmptr)){
+        printf("1\n");
+        if ( cmptr->cmsg_level != IPPROTO_IP || cmptr->cmsg_type != IP_PKTINFO ){
+            continue;
+        }
+        struct in_pktinfo *pi = (struct in_pktinfo *)CMSG_DATA(cmptr);
+        if(pktp){
+            printf("copy\n");
+            memcpy(pktp, pi, sizeof(*pi));
+        }
+    }
+    opt = 0;
+    setsockopt(fd, IPPROTO_IP, IP_PKTINFO, &opt, sizeof (opt));
+    return n;
+}
+
+
+
+static pthread_key_t readLineKey;
+static pthread_once_t readLineOnce = PTHREAD_ONCE_INIT;
+
+static void Readline_destructor(void *ptr){
+    free(ptr);
+}
+
+int Pthread_key_create(pthread_key_t *keyptr, void (*destructor)(void*)){
+    if(pthread_key_create(keyptr, destructor) != 0)
+        err("Pthread_key_create");
+}
+
+int Pthread_once(pthread_once_t *onceptr, void (*init)(void)){
+    if(pthread_once(onceptr, init) != 0)
+        err("Pthread_key_create");
+}
+
+static void Readline_once(){
+    Pthread_key_create(&readLineKey, Readline_destructor);
+}
+
+static size_t My_read_r(Rline *rline,int fd, char *vptr){
+    while(readCnt <= 0){
+        if((readCnt = read(fd, rline->buf, MAXLINE)) < 0){
+            if(errno == EINTR){
+                continue;
+            }
+            else{
+                printf("readCnt is %d \n", -1);
+                return -1;
+            }
+        }else if(0 == readCnt){
+            return 0;
+        } 
+        rline->bufptr = readBuf;
+    }
+    --readCnt;
+    *vptr = *(readPtr++);
+    return 1;
+}
+
+size_t Readline_r(int fd,void *vptr, size_t maxLen){
+    size_t ret = 0;
+    size_t readn = 0;
+    char *ptr = vptr;
+    char c;
+    Rline *rline;
+    Pthread_once(&readLineOnce, Readline_once);
+    if((rline = pthread_getspecific(readLineKey)) == NULL){
+        rline = calloc(1, sizeof(Rline));
+        pthread_setspecific(readLineKey, rline);
+    }
+
+    while(ret < maxLen){
+        if((readn = My_read_r(rline, fd, &c)) == 1){
+            *(ptr++) = c;
+            ++ret;
+            if(c == '\n')
+                break;
+        }else if(0 == readn){
+            *ptr = 0;
+            return ret;
+        }else if(errno != EINTR){
+            return -1;
+        }
+    }
+    *ptr = 0;
+    return ret;
+}
+
--- wrap_func.h
+++ wrap_func.h
@@ -18,27 +18,84 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+#include <sys/un.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <net/route.h>
+#include <linux/pfkeyv2.h>
+#include <sys/param.h>
+#include <linux/in.h>
+#include <pthread.h>
 
 
 
 #define MAXLINE 4096
 #define MAXFD 64
 
+#ifndef WRAPFUNC
+extern int err_flag;
+#endif
+
 #define err(s) do{  \
-    printf(s); \
-    printf(" %s %s %d\n", strerror(errno), __FILE__, __LINE__); \
+    if(err_flag == 1){ \
+        syslog(LOG_ERR,s); \
+        syslog(LOG_ERR," %s %s %d\n", hstrerror(errno), __FILE__, __LINE__); \
+    }else{ \
+        printf(s); \
+        printf(" %s %s %d\n", hstrerror(errno), __FILE__, __LINE__); \
+    } \
     exit(1); \
 }while(0)
 
 #define err_msg(s) do{  \
-    printf(s); \
-    printf(" %s %s %d\n", hstrerror(errno), __FILE__, __LINE__); \
-    exit(1); \
+    if(err_flag == 1){ \
+        syslog(LOG_ERR,s); \
+        syslog(LOG_ERR," %s %s %d\n", hstrerror(errno), __FILE__, __LINE__); \
+    }else{ \
+        printf(s); \
+        printf(" %s %s %d\n", hstrerror(errno), __FILE__, __LINE__); \
+    } \
 }while(0)
+
+struct UnpInPktinfo{
+    struct in_addr addr;
+    int ifindex;
+};
+
+typedef struct sockaddr SA;
 
 typedef void (Signal_func)(int);
 
+#define IFI_NAME_LEN 16
+#define IFI_HADDR_LEN 8
+
+
+#define IFI_NAME 16
+#define IFI_HADDR 8
+#define IFI_ALIAS 1
+
+struct IfiInfo{
+    char name[IFI_NAME_LEN];
+    unsigned int index;
+    unsigned int mtu;
+    unsigned char haddr[IFI_HADDR_LEN];
+    unsigned short hlen;
+
+    short flags;
+
+    short myFlags;
+    struct sockaddr *addr;
+    struct sockaddr *baddr;
+    struct sockaddr *detaddr;
+    struct IfiInfo *next;
+};
+
+
+int Min(int a, int b);
 void Pthread_mutex_lock(pthread_mutex_t *mptr);
 int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 int Listen(int sockfd, int backlog);
@@ -67,4 +124,29 @@ int Tcp_listen(const char *hostname, const char *service, socklen_t *addrlenp, i
 int Udp_client(const char *hostname, const char *service, struct sockaddr **saptr, socklen_t *lenp, int family);
 int Udp_client_connect(const char *hostname, const char *service, int family);
 int Udp_server(const char *hostname, const char *service, socklen_t *plen, int family);
+int daemon_init(const char *pname, int faclity);
+int Readable_timeo(int fd, int sec);
+int Socketpair(int domain, int type, int protocol, int sv[2]);
+ssize_t Recvmsg(int sockfd, struct msghdr *msg, int flags);
+ssize_t Sendmsg(int sockfd, const struct msghdr *msg, int flags);
+int Connect_nonb(int socketFd, struct sockaddr *addr, socklen_t alen, int nsec);
+struct IfiInfo* Get_ifi_info(int family, int doaliases);
+void Free_ifi_info(struct IfiInfo *ifihead);
+int Family_to_level(int family);
+int Mcast_join(int sock, const struct sockaddr *grp, socklen_t grplen, const char *ifname, unsigned int ifindex);
+int Sockfd_to_family(int sockfd);
+int Mcast_set_loop(int sock, int onoff);
+ssize_t Recv_des(int fd, void *ptr, size_t nbytes,SA *sa, socklen_t *alen,struct in_pktinfo *pktp);
+
+
+typedef struct Rline_s{
+    int cnt;
+    char *bufptr;
+    char buf[MAXLINE];
+}Rline;
+
+int Pthread_key_create(pthread_key_t *keyptr, void (*destructor)(void*));
+int Pthread_once(pthread_once_t *onceptr, void (*init)(void));
+static size_t My_read_r(Rline *rline,int fd, char *vptr);
+size_t Readline_r(int fd,void *vptr, size_t maxLen);
 #endif
